5f690ccea86c4e89cfc2baa4587567c939ee7c52
Hexatonix72 <33948652+Hexatonix72@users.noreply.github.com>
2024-09-24 19:58:34 -0400

    .

--- MainProgram.py
+++ MainProgram.py
@@ -14,6 +14,19 @@ class Task:
         self.createdAt = createdAt
         self.updatedAt = updatedAt
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'description': self.description,
+            'status': self.status,
+            'createdAt': self.createdAt,
+            'updatedAt': self.updatedAt
+        }
+    def list_to_json(task_list, file_path):
+        with open(file_path, 'w') as json_file:
+            json.dump([task.to_dict() for task in task_list], json_file, indent=4)
+
+
 task_list = []
 
 def addTask():
@@ -21,10 +34,11 @@ def addTask():
     taskNumber = number_of_tasks + 1
     name = input("Please enter the name of your task: ")
     status = "todo"
-    createdTime = datetime.now()
-    updatedTime = datetime.now()
+    createdTime = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+    updatedTime = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+    task = Task(taskNumber, name, status, createdTime, updatedTime)
 
-    task_list.append(Task(taskNumber, name, status, createdTime, updatedTime))
+    task_list.append(task)
 
 def print_objects_with_attributes(object_list):
     for i, obj in enumerate(object_list):
@@ -38,6 +52,5 @@ def print_objects_with_attributes(object_list):
 addTask()
 addTask()
 print_objects_with_attributes(task_list)
-
-with open('listOfTasks.json', 'w') as f:
-    json.dump(task_list, f, indent=4)
+Task.list_to_json(task_list, "task_list.json")
+print(task_list)
